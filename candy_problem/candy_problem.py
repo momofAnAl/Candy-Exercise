0f62e634a4b7fe4093a7b7dea06cabf1288aeb0f
anhtran <anhtr077@gmail.com>
2024-09-20 08:19:02 -0700

    update functions and tests

--- candy_problem/candy_problem.py
+++ candy_problem/candy_problem.py
@@ -9,9 +9,28 @@ friend_favorites = [
     ["Arlene", ["chocolate bar", "milky way", "laffy taffy" ]],
     ["Carlie", ["nerds", "sour patch kids", "laffy taffy" ]]
 ]
-'''
+# '''
+friend_favorites = [
+    ["Sally", ["lollipop", "bubble gum", "laffy taffy" ]],
+    ["Bob", ["milky way", "licorice", "lollipop" ]],
+    ["Arlene", ["chocolate bar", "milky way", "laffy taffy" ]],
+    ["Carlie", ["nerds", "sour patch kids", "laffy taffy" ]]
+]
 def get_friends_favorite_candy_count(favorites):
-    pass
+    candies_counting = {}
+    
+    for friends_favorites in favorites:
+        friend_name = friends_favorites[0]
+        candies_name = friends_favorites[1]
+        for candy in candies_name: 
+            if candy not in candies_counting:
+                candies_counting[candy] = 1
+            else:
+                candies_counting[candy] += 1
+    
+    return candies_counting
+get_friends_favorite_candy_count(friend_favorites)
+    
 
 '''
 2. 
@@ -27,15 +46,31 @@ friend_favorites = [
 ]
 '''
 def create_new_candy_data_structure(data):
-    pass 
-
+    candies_favorite = {}
+    
+    for friends, candies in friend_favorites:
+        for candy in candies: 
+            if candy not in candies_favorite:
+                candies_favorite[candy] = [friends]
+            else: 
+                candies_favorite[candy].append(friends)
+                
+    return candies_favorite           
+        
+print(create_new_candy_data_structure(friend_favorites))
+        
 '''
 3. 
 In `get_friends_who_like_specified_candy()`, return a tuple of 
 friends who like the candy specified in the candy_name parameter.
 '''
 def get_friends_who_like_specific_candy(data, candy_name):
-    pass
+    
+    candy_favorite = create_new_candy_data_structure(data)
+    tuple_candies = tuple(candy_favorite[candy_name])
+    return tuple_candies
+
+print(get_friends_who_like_specific_candy(friend_favorites, "nerds"))
 
 '''
 4. 
@@ -43,7 +78,15 @@ In, `create_candy_set()`, return a set of all the candies from
 the data structure made in `create_new_candy_data_structure()`.
 '''
 def create_candy_set(data):
-    pass 
+    candies_storage = create_new_candy_data_structure(data)
+    new_candies_storage = set()
+    
+    for candies in candies_storage.keys():
+        new_candies_storage.add(candies)
+     
+    return new_candies_storage
+
+print(create_candy_set(friend_favorites))       
 
 '''
 5. 
--- tests/test_candy_data_structure.py
+++ tests/test_candy_data_structure.py
@@ -33,13 +33,118 @@ def test_create_candy_data_structure_values():
     
     # Assert
     assert len(new_candy_data) == 8
-    '''
-    Add your assertions here!
-    '''
-
+    assert type(new_candy_data) == dict
+    assert len(new_candy_data["lollipop"]) == 2
 '''
 5. 
 Starting with nominal cases, write tests for each of the functions 
 in the file tests/test_candy_data_structure.py then write tests to 
 handle edge cases.
 '''
+def test_get_friends_favorite_candy_count():
+    
+    #Arrange
+    favorites = [
+        ["Sally", [ "lollipop", "bubble gum", "laffy taffy"]],
+        [ "Bob", ["milky way", "licorice", "lollipop"]],
+        [ "Arlene", ["chocolate bar", "milky way", "laffy taffy"]],
+        [ "Carlie", ["nerds", "sour patch kids", "laffy taffy"]]
+    ]
+    expected = {
+        'lollipop': 2, 
+        'bubble gum': 1, 
+        'laffy taffy': 3, 
+        'milky way': 2, 
+        'licorice': 1, 
+        'chocolate bar': 1, 
+        'nerds': 1, 
+        'sour patch kids': 1
+    }
+    #Act
+    result = get_friends_favorite_candy_count(favorites)
+    
+    #Assert
+    assert  result == expected
+    assert type(result) == dict
+    assert len(result) == 8
+    
+def test_get_friends_favorite_candy_count_empty():
+    
+    #Arrange
+    favorites = []
+    #Act
+    result = get_friends_favorite_candy_count(favorites)
+    
+    
+    #Assert
+    assert result == {}
+    
+def test_create_new_candy_data_structure():
+    
+    #Arrange
+    favorites = [
+    ["Sally", ["lollipop", "bubble gum", "laffy taffy" ]],
+    ["Bob", ["milky way", "licorice", "lollipop" ]],
+    ["Arlene", ["chocolate bar", "milky way", "laffy taffy" ]],
+    ["Carlie", ["nerds", "sour patch kids", "laffy taffy" ]]
+    ]
+    expected = {
+        'lollipop': ['Sally', 'Bob'], 
+        'bubble gum': ['Sally'], 
+        'laffy taffy': ['Sally','Arlene', 'Carlie'], 
+        'milky way': ['Bob', 'Arlene'], 
+        'licorice': ['Bob'], 
+        'chocolate bar': ['Arlene'], 
+        'nerds': ['Carlie'], 
+        'sour patch kids': ['Carlie']
+    }
+    
+    #Act
+    result = create_new_candy_data_structure(favorites)
+    
+    #Assert
+    assert result == expected
+    assert type(result) == dict
+    assert len(result) == 8
+    
+
+def test_get_friends_who_like_specific_candy():
+    
+    #Arrange
+    favorites = [
+    ["Sally", ["lollipop", "bubble gum", "laffy taffy" ]],
+    ["Bob", ["milky way", "licorice", "lollipop" ]],
+    ["Arlene", ["chocolate bar", "milky way", "laffy taffy" ]],
+    ["Carlie", ["nerds", "sour patch kids", "laffy taffy" ]]
+    ]
+    
+    candy_type = "laffy taffy"
+    
+    ##Act
+    result = get_friends_who_like_specific_candy(favorites, candy_type)
+    
+    ##Assert
+    assert result == ('Sally', 'Arlene', 'Carlie')
+    assert type(result) == tuple
+    assert len(result) == 3
+    
+def create_candy_set():
+    
+    #Arrange
+    favorites = [
+    ["Sally", ["lollipop", "bubble gum", "laffy taffy" ]],
+    ["Bob", ["milky way", "licorice", "lollipop" ]],
+    ["Arlene", ["chocolate bar", "milky way", "laffy taffy" ]],
+    ["Carlie", ["nerds", "sour patch kids", "laffy taffy" ]]
+    ]
+    
+    expected = {'sour patch kids', 'bubble gum', 'chocolate bar', \
+        'nerds', 'licorice', 'milky way', 'laffy taffy', 'lollipop'}
+    
+    #Act 
+    result = create_candy_set (favorites)
+    
+    #Assert
+    assert result == expected
+    assert len(result) == 8
+    assert type(result) == set
